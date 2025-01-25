951f25b47ab9358e5c5f20c621e4d2c206e81d34
Jonas Attrup <attrup.jonas@gmail.com>
2025-01-25 15:12:03 +0100

    Updated CLI arguments and descriptions

--- src/pdf-merge/Program.cs
+++ src/pdf-merge/Program.cs
@@ -3,15 +3,17 @@
 namespace pdf_merge {
     class Program {
         public class Options {
-            [Option('d', "directory", Required = false, HelpText = "Use this to specify a directory from which to retrieve all PDF files.")]
+            [Option('d', "directory", Required = false, HelpText = "Specify a directory from which to retrieve all PDF files (Files are sorted lexicographically before combining).")]
             public string? Directory { get; set; }
 
-            [Option('f', "files", Required = false, HelpText = "Use this to specify PDF file paths individually (accepts multiple file paths separated by spaces).")]
+            [Option('f', "files", Required = false, HelpText = "Specify PDF file paths individually (Accepts multiple file paths separated by spaces) (Files are combined in the order they are specified).")]
             public IEnumerable<string>? Files { get; set; }
 
-            [Option('o', "output", Required = false, HelpText = "Use this to specify the output file name.")]
+            [Option('o', "output", Required = false, HelpText = "Set the output file name without file extension (Default is 'combined').")]
             public string? Output { get; set; }
 
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
 
             static void Main(string[] args) {
                 Parser.Default.ParseArguments<Options>(args)
