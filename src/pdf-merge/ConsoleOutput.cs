c9d4fe5c462269ba953d6b5ca7899e8398fb6fcd
Jonas Attrup <attrup.jonas@gmail.com>
2025-01-29 09:30:13 +0100

    Method Annotations for ConsoleOutput.cs

--- src/pdf-merge/ConsoleOutput.cs
+++ src/pdf-merge/ConsoleOutput.cs
@@ -4,19 +4,36 @@ using System.Text;
 namespace pdf_merge;
 
 public class ConsoleOutput {
-
+    /// <summary>
+    /// Prints a "Success" message to the CLI, i.e. the method argument `message` prepended with "SUCCESS"
+    /// </summary>
+    /// <param name="message">Message to print to console</param>
     public static void Success(string message) {
         Console.WriteLine($"SUCCESS: {message}");
     }
 
+    /// <summary>
+    /// Prints a "Warning" message to the CLI, i.e. the method argument `message` prepended with "WARNING"
+    /// </summary>
+    /// <param name="message">Message to print to console</param>
     public static void Warning(string message) {
         Console.WriteLine($"WARNING: {message}");
     }
 
+    /// <summary>
+    /// Prints a "Error" message to the CLI, i.e. the method argument `message` prepended with "ERROR"
+    /// </summary>
+    /// <param name="message">Message to print to console</param>
     public static void Error(string message) {
         Console.WriteLine($"ERROR: {message}");
     }
 
+    /// <summary>
+    /// Prints a Progress bar to the CLI as well as a message to explain the progress bar
+    /// </summary>
+    /// <param name="current">Current progress</param>
+    /// <param name="target">Target progress / final value</param>
+    /// <param name="message">Message to print along with progress bar</param>
     public static void ProgressBar(int current, int target, string message) {
         StringBuilder sb = new StringBuilder($"{message} [", 128);
         float status = (float)current / (float)target * 100.0f;
@@ -41,6 +58,10 @@ public class ConsoleOutput {
         }
     }
 
+    /// <summary>
+    /// Prints a message to console. Equal to calling Console.WriteLine
+    /// </summary>
+    /// <param name="message">Message to print to console</param>
     public static void Print(string message) {
         Console.WriteLine(message);
     }
