341517612da553b87bdfe6fc6222ede751615033
Jonas Attrup <attrup.jonas@gmail.com>
2025-01-29 09:41:53 +0100

    Method Annotations for FileMerger.cs and FileParser.cs

--- src/pdf-merge/FileMerger.cs
+++ src/pdf-merge/FileMerger.cs
@@ -5,6 +5,15 @@ using PdfSharp.Pdf.IO;
 namespace pdf_merge;
 
 public class FileMerger {
+
+    /// <summary>
+    /// Merges a list of PDF files into a single PDF file with the name "<outputName>.pdf". 
+    /// All the specified files must exist, otherwise this method will throw an IOException.
+    /// </summary>
+    /// <param name="files">The list of paths for each of the PDF files to merge</param>
+    /// <param name="outputName">Name of merged PDF file</param>
+    /// <param name="enableVerbose">Enables verbose mode to print extra messages to console</param>
+    /// <exception cref="IOException"></exception>
     public static void MergeFiles(List<string> files, string outputName, bool enableVerbose) {
         // Create Output PDF document
         PdfDocument outPdf = new PdfDocument();
@@ -22,6 +31,11 @@ public class FileMerger {
         if (enableVerbose) ConsoleOutput.Success($"PDF files merged and saved as {outputName + ".pdf"}");
     }
 
+    /// <summary>
+    /// Copies all pages from the `source` PDF document to the end of the `target` document in order
+    /// </summary>
+    /// <param name="source">PDF document to copy pages from</param>
+    /// <param name="target">PDF document to copy pages to</param>
     private static void CopyPages(PdfDocument source, PdfDocument target) {
         // Copy each page in the source file to the target file
         for (int i = 0; i < source.PageCount; i++) {
--- src/pdf-merge/FileParser.cs
+++ src/pdf-merge/FileParser.cs
@@ -4,6 +4,16 @@ using System.IO;
 namespace pdf_merge;
 
 public class FileParser {
+
+    /// <summary>
+    /// Retrieves all PDF files from a specified directory and adds them to the referenced filePaths list.  
+    /// Throws an exception in case the directory path is invalid.
+    /// Prints a warning to the CLI in case the specified directory is empty.
+    /// </summary>
+    /// <param name="dirPath">Path to the directory to retrieve files from</param>
+    /// <param name="filePaths">List to extend with the valid PDF file paths</param>
+    /// <param name="enableVerbose">Enables verbose mode to print extra messages to console</param>
+    /// <exception cref="IOException"></exception>
     public static void FilesFromDirectory(string dirPath, ref List<string> filePaths, bool enableVerbose) {
         // Check that the provided path is a valid path to a directory
         if (!Directory.Exists(dirPath)) {
@@ -32,6 +42,14 @@ public class FileParser {
         if (enableVerbose) ConsoleOutput.Success($"Finished retrieving files from target directory");
     }
 
+    /// <summary>
+    /// Validates that a list of specified files are PDF format and that the paths are valid.
+    /// Throws an exception if a specified file is not PDF or if a path is invalid.
+    /// </summary>
+    /// <param name="files">List of file paths to validate</param>
+    /// <param name="filePaths">List to extend with the valid PDF file paths</param>
+    /// <param name="enableVerbose">Enables verbose mode to print extra messages to console</param>
+    /// <exception cref="IOException"></exception>
     public static void FilesFromList(IEnumerable<string> files, ref List<string> filePaths, bool enableVerbose) {
         foreach (string file in files) {
             // Check that the file is PDF and exists
